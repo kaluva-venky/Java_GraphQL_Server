9340bbfd635a98d6692987d4ab83330215e14400
Venky <18035237+kaluva-venky@users.noreply.github.com>
2020-06-10 00:37:45 -0500

    Spring starter template

---
+++ src/main/java/dev/venky/JavaGraphQlServerApplication.java
@@ -0,0 +1,13 @@
+package dev.venky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphQlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGraphQlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/venky/JavaGraphQlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.venky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphQlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
